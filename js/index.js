2edf907fae3c3ecb6936ffeb852b7bf006e53685
Panagiotis Pantouveris <p.pantouveris@gmail.com>
2016-09-19 20:01:23 +0300

    added parallax and openlayers

--- js/index.js
+++ js/index.js
@@ -1,8 +1,31 @@
-$(document).on('click', 'a:not([target="_blank"])', function(event){
-    event.preventDefault();
+$(document).ready(function() {
 
-    $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 700);
+	//on screen size smalled that 767px remove parallax images.
+	$(window).resize(function () {
+	    // Check window size
+	    if($(window).width() < 767) {
+	        // Unset data-image-src property
+	        $(".parallax-window").attr("data-image-src", "");
+	    }
+	});
+
+	//scrolling effect for all anhors except the ones with target _blank.
+	$(document).on('click', 'a:not([target="_blank"])', function(event){
+	    event.preventDefault();
+
+		$('html, body').animate({
+	    	scrollTop: $( $.attr(this, 'href') ).offset().top
+		}, 700);
+	});
+
+	//redirect button to map.
+	$("#redirect").click(function () {
+	    window.location.href ="https://panospant.github.io/partials/map.html";
+	 });
 });
+
+
+
+
+
 
---
+++ js/map.js
@@ -0,0 +1,190 @@
+//initializing variables.
+var peristeri = ol.proj.fromLonLat([23.6903,38.007525]);
+var piraeus = ol.proj.fromLonLat([23.652674,37.94171]);
+var zagreb = ol.proj.fromLonLat([15.978928,45.800084]);
+var athens = ol.proj.fromLonLat([23.7275,37.9838]);
+var pentagon = ol.proj.fromLonLat([23.782665,37.999707]);
+var buttonPeristeri = $('<button>Start the Journey!</button>');
+var buttonPiraeus = $('<button>Studied in University of Piraeus from 2009 to 2012 and 2013 to 2015. Wonder where I was from 2012 to 2013?</button>');
+var buttonZagreb = $('<button>Participated in the Erasmus program for both studies and internship. Next station?</button>');
+var buttonAthens = $('<button>Back home! The last 6 months of my studies I was working in Velti, while finishing my thesis. Now for the final stop...</button>'); 
+var buttonPentagon = $('<strong><i>Worked as a programmer for the Hellenic Army at the Center of Information Support. Here our journey ends and we reach the present!</strong></i>');
+
+var view = new ol.View({
+  // the view's initial state
+  center: peristeri,
+  zoom: 10
+});
+
+var peristeriFeature = new ol.Feature({
+  geometry: new ol.geom.Point(peristeri),
+  name: 'Peristeri'
+});
+
+var piraeusFeature = new ol.Feature({
+  geometry: new ol.geom.Point(piraeus),
+  name: 'Piraeus'
+});
+
+var zagrebFeature = new ol.Feature({
+  geometry: new ol.geom.Point(zagreb),
+  name: 'Zagreb'
+});
+
+var athensFeature = new ol.Feature({
+  geometry: new ol.geom.Point(athens),
+  name: 'Athens'
+});
+
+var pentagonFeature = new ol.Feature({
+  geometry: new ol.geom.Point(pentagon),
+  name: 'Pentagon'
+});
+
+var iconStyle = new ol.style.Style({
+  image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
+    anchor: [0.5, 46],
+    anchorXUnits: 'fraction',
+    anchorYUnits: 'pixels',
+    src: 'http://openlayers.org/en/v3.18.2/examples/data/icon.png'
+  }))
+});
+
+peristeriFeature.setStyle(iconStyle);
+piraeusFeature.setStyle(iconStyle);
+zagrebFeature.setStyle(iconStyle);
+athensFeature.setStyle(iconStyle);
+pentagonFeature.setStyle(iconStyle);
+
+var vectorSource = new ol.source.Vector({
+  features: [peristeriFeature]
+});
+
+var vectorLayer = new ol.layer.Vector({
+  source: vectorSource
+});
+
+var map = new ol.Map({
+  layers: [
+  new ol.layer.Tile({
+    preload: 4,
+    source: new ol.source.OSM()
+  }), vectorLayer
+  ],
+  // Improve user experience by loading tiles while animating. Will make
+  // animations stutter on mobile or slow devices.
+  loadTilesWhileAnimating: true,
+  target: 'map',
+  controls: ol.control.defaults({
+    attributionOptions: /** @type {olx.control.AttributionOptions} */ ({
+      collapsible: false
+    })
+  }),
+  view: view
+});
+
+var element = document.getElementById('popup');
+
+var popup = new ol.Overlay({
+  element: element,
+  positioning: 'bottom-center',
+  stopEvent: false,
+  offset: [0, -50]
+});
+map.addOverlay(popup);
+
+// display popup on click
+map.on('click', function(evt) {
+  var feature = map.forEachFeatureAtPixel(evt.pixel,
+    function(feature) {
+      return feature;
+    });
+    if (feature) {
+      var coordinates = feature.getGeometry().getCoordinates();
+      popup.setPosition(coordinates);
+      //check feature name and display according info.
+      if(feature.get('name') === 'Peristeri'){
+        $(element).popover({
+          'placement': 'top',
+          'html': true,
+          'content': buttonPeristeri
+        });
+      } else if(feature.get('name') === 'Piraeus'){
+        $(element).popover({
+          'placement': 'top',
+          'html': true,
+          'content': buttonPiraeus
+        });
+      } else if(feature.get('name') === 'Zagreb'){
+        $(element).popover({
+          'placement': 'top',
+          'html': true,
+          'content': buttonZagreb
+        });
+      } else if(feature.get('name') === 'Athens'){
+        $(element).popover({
+          'placement': 'top',
+          'html': true,
+          'content': buttonAthens
+        });
+      } else if(feature.get('name') === 'Pentagon'){
+        $(element).popover({
+          'placement': 'top',
+          'html': true,
+          'content': buttonPentagon
+        });
+      }
+      $(element).popover('show');
+    } else {
+      $(element).popover('destroy');
+  }
+});
+
+//Functions for switching from a place to another.
+function go(location, remove, add){
+  var pan = ol.animation.pan({
+    duration: 1000,
+    source: /** @type {ol.Coordinate} */ (view.getCenter())
+  });
+  map.beforeRender(pan);
+  view.setCenter(location);
+
+  vectorSource.removeFeature(remove);
+  vectorSource.addFeature(add);
+};
+
+function fly(location, remove, add){
+  var duration = 6000;
+  var start = +new Date();
+  var pan = ol.animation.pan({
+    duration: duration,
+    source: /** @type {ol.Coordinate} */ (view.getCenter()),
+    start: start,
+  });
+  var bounce = ol.animation.bounce({
+    duration: duration,
+    resolution: 4 * view.getResolution(),
+    start: start,
+  });
+  map.beforeRender(pan, bounce);
+  view.setCenter(location);
+
+  vectorSource.removeFeature(remove);
+  vectorSource.addFeature(add);
+}
+
+buttonPeristeri.click(function() {
+  go(piraeus, peristeriFeature, piraeusFeature);
+});
+
+buttonPiraeus.click(function(){
+  fly(zagreb, piraeusFeature, zagrebFeature);
+});
+
+buttonZagreb.click(function(){
+  fly(athens, zagrebFeature, athensFeature);
+});
+
+buttonAthens.click(function() {
+  go(pentagon, athensFeature, pentagonFeature);
+});
